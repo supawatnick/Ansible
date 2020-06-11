2b47ed5aaf8bcf3a88c136841418c441b164df15
Supawat <supawat.nick@gmail.com>
2020-06-11 12:39:35 +0700

    ok

--- DR Activation/Test Plan/1. Activate Test Plan/snapmirrorupdate.sh
+++ DR Activation/Test Plan/1. Activate Test Plan/snapmirrorupdate.sh
@@ -7,17 +7,19 @@ ontap_drhostname=`cat vars.yaml | shyaml get-value ontap_drhostname`
 ontap_drusername=`cat vars.yaml | shyaml get-value ontap_drusername`
 ontap_drvserver=`cat vars.yaml | shyaml get-value ontap_drvserver`
 
+# Create Loop following volumes no.
 FILEIN=vol_dest.list
-LOOPS=`wc -l $FILEIN |awk '{print $1}'`
+LOOPS=`wc -l $FILEIN |awk '{print $1}'` ## Count no. of volume for loop
 echo $LOOPS "RELATION TO EXECUTE"
 COUNT=1
 
 ## LOOP TO REGISTER VMX ON ALL DEVICE ###
 while [ $COUNT -le $LOOPS ]
 do
-VOL_DEST=`head -$COUNT $FILEIN |tail -1|awk '{print $1}'`
+VOL_DEST=`head -$COUNT $FILEIN |tail -1|awk '{print $1}'`  ## Put Volume dest to VOL_DEST
 echo ssh $ontap_drusername@$ontap_drhostname "snapmirror update $ontap_drvserver:$VOL_DEST"
 sleep 3
 (( COUNT++ ))
 done
 
+rm vol_dest.list
