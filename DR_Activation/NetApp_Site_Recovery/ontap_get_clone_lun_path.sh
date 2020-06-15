8008eaef1039bb09e5104fe9c8ee9393b4a248b2
Supawat <supawat.nick@gmail.com>
2020-06-15 22:41:29 +0700

    Update ontap_get_clone_lun_path.sh

--- DR_Activation/NetApp_Site_Recovery/ontap_get_clone_lun_path.sh
+++ DR_Activation/NetApp_Site_Recovery/ontap_get_clone_lun_path.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Need to install shyaml
 
-# Get DR volume name from vars.yaml ###
-
+# Get ONTAP var
 ontap_drhostname=`cat vars.yaml | shyaml get-value ontap_drhostname`
 ontap_drusername=`cat vars.yaml | shyaml get-value ontap_drusername`
 ontap_drvserver=`cat vars.yaml | shyaml get-value ontap_drvserver`
 
-lun show -vserver svm2 -path /vol/clone* > lun_clone.list
+# Get clone LUNs and export to lun_clone.list
+ssh $ontap_drusername@$ontap_drhostname "lun show -vserver $ontap_drvserver -path /vol/clone*" > lun_clone.list
 
+# Create loop following no. of LUNs clone
 FILEIN=lun_clone.list
 LOOPS=`wc -l $FILEIN |awk '{print $2}'`
 echo Found $LOOPS Clone LUN
 COUNT=1
 
-
+# Create lun_clone.yaml
 echo "ontap_cloneluns:" > lun_clone.yaml
 while [ $COUNT -le $LOOPS ]
 do
@@ -22,3 +23,5 @@ LUN_CLONE=`head -$COUNT $FILEIN |tail -1|awk '{print $2}'`
 echo " - " $LUN_CLONE >> lun_clone.yaml
 (( COUNT++ ))
 done
+
+rm lun_clone.list
